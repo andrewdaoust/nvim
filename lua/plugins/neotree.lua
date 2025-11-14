75399407ff7f4a713fca95bd33493d3d09f32467
andrewdaoust <daoust.andrew@gmail.com>
2025-11-14 14:31:31 -0500

    Update neotree config to close tree on file open

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,14 +1,27 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    config = function()
-        vim.keymap.set('n', "<leader>nt", ":Neotree filesystem reveal left<CR>", {})
-    end
-}
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons", -- optional, but recommended
+	},
+	lazy = false, -- neo-tree will lazily load itself
+	config = function()
+		vim.keymap.set("n", "<leader>nt", ":Neotree filesystem reveal left<CR>", {})
+		require("neo-tree").setup({
+			event_handlers = {
 
+				{
+					event = "file_open_requested",
+					handler = function()
+						-- auto close
+						-- vim.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
+	end,
+}
